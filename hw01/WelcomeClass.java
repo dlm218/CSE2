045e0ff3599a68b9494b4635510fd41a7fd9f549
dlm218 <dlm218@lehigh.edu>
2014-08-28 00:08:36 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////
+//David Mugavero
+//hw01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+//  print the statement with box around it
+    System.out.println("  -----------");
+    System.out.println("  | Welcome |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-D--L--M--2--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This summer my friend and I stayed in Baltimore for the weekend");
+    
+    //test/testtest
+    }
+}
